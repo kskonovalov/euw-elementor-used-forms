163707af97b2ecd6167da375cb78c4e92b82a4ba
Konstantin <picprofit@gmail.com>
2021-05-24 20:51:00 +0300

    add: more useful fields added

--- euf-elementor-used-forms.php
+++ euf-elementor-used-forms.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: EUF Elementor used forms
- * Description: Displays the forms which are currently used by elementor
+ * Description: Displays the forms which are currently used by Elementor
  * Plugin URI:  https://github.com/kskonovalov/euf-elementor-used-forms
  * GitHub Plugin URI: https://github.com/kskonovalov/euf-elementor-used-forms
  * Version: 0.1.0
@@ -89,7 +89,14 @@ function euf_main_func() {
         'orderby'             => 'date',
         'order'               => 'DESC',
         'post_type'           => $usedPostTypes,
-        'post_status'         => array( 'publish', 'pending', 'draft', 'auto-draft', 'future', 'private' ),
+        'post_status'         => array(
+          'publish',
+          'pending',
+          'draft',
+          'auto-draft',
+          'future',
+          'private'
+        ),
         'exclude_from_search' => false
       ] );
 
@@ -110,10 +117,11 @@ function euf_main_func() {
       $titleRecaptcha  = __( "Recaptcha", euf_title() );
       $titleRecaptcha3 = __( "Recaptcha v3", euf_title() );
       $titleHoneypot   = __( "Honeypot", euf_title() );
+      $titleSavesToDb   = __( "Collect submissions", euf_title() );
       $titleEmail2     = __( "E-mail to 2", euf_title() );
       $titleActions    = __( "Actions", euf_title() );
-      $check = "<span style='color:green; font-size: 30px;'>&check;</span>";
-      $times = "<span style='color:#530101; font-size: 26px;'>&times;</span>";
+      $check           = "<span style='color:green; font-size: 30px;'>&check;</span>";
+      $times           = "<span style='color:#530101; font-size: 26px;'>&times;</span>";
       echo '<table cellspacing="0" cellpadding="0" class="widefat fixed" style="width: 800px; max-width: 100%;">';
       echo "<thead><tr>";
       echo "<th class='manage-column'><b>{$titleForm}</b></th>";
@@ -122,6 +130,7 @@ function euf_main_func() {
       echo "<th class='manage-column'><b>{$titleRecaptcha}</b></th>";
       echo "<th class='manage-column'><b>{$titleRecaptcha3}</b></th>";
       echo "<th class='manage-column'><b>{$titleHoneypot}</b></th>";
+      echo "<th class='manage-column'><b>{$titleSavesToDb}</b></th>";
       echo "<th class='manage-column'><b>{$titleActions}</b></th>";
       echo "</tr></thead><tbody>";
       foreach ( $posts as $post ) {
@@ -136,34 +145,42 @@ ROW;
             $formName = $form["settings"]["form_name"];
             $emailTo  = $form["settings"]["email_to"];
             $emailTo2 = "";
-            if(isset($form["settings"]["submit_actions"]) && in_array("email2", $form["settings"]["submit_actions"])) {
+            if ( isset( $form["settings"]["submit_actions"] ) && in_array( "email2", $form["settings"]["submit_actions"] ) ) {
               $emailTo2 = $form["settings"]["email_to_2"];
             }
             $formFields = [];
-            foreach($form["settings"]["form_fields"] as $field) {
-                if(isset($field["field_label"]) && !empty($field["field_label"])) {
-                    $formFields[] = $field["field_label"];
-                } else if(isset($field["placeholder"]) && !empty($field["placeholder"])) {
+            foreach ( $form["settings"]["form_fields"] as $field ) {
+              if ( isset( $field["field_label"] ) && ! empty( $field["field_label"] ) ) {
+                $formFields[] = $field["field_label"];
+              } else {
+                if ( isset( $field["placeholder"] ) && ! empty( $field["placeholder"] ) ) {
                   $formFields[] = $field["placeholder"];
-                } else if(isset($field["field_type"]) && !empty($field["field_type"])) {
-                  $formFields[] = $field["field_type"];
+                } else {
+                  if ( isset( $field["field_type"] ) && ! empty( $field["field_type"] ) ) {
+                    $formFields[] = $field["field_type"];
+                  }
                 }
+              }
             }
-            $recaptcha = in_array("recaptcha", $formFields) ? $check : $times;
-            $recaptcha3 = in_array("recaptcha_v3", $formFields) ? $check : $times;
-            $honeypot = in_array("honeypot", $formFields) ? $check : $times;
+            $recaptcha  = in_array( "recaptcha", $formFields ) ? $check : $times;
+            $recaptcha3 = in_array( "recaptcha_v3", $formFields ) ? $check : $times;
+            $honeypot   = in_array( "honeypot", $formFields ) ? $check : $times;
 
             $formActions = "email";
-            if(isset($form["settings"]["submit_actions"]) && is_array($form["settings"]["submit_actions"])) {
+            $collectSubmissions = $times;
+            if ( isset( $form["settings"]["submit_actions"] ) && is_array( $form["settings"]["submit_actions"] ) ) {
               $formActions = implode( ", ", $form["settings"]["submit_actions"] );
+              $collectSubmissions = in_array( "save-to-database", $form["settings"]["submit_actions"] ) ? $check : $times;
             }
+
             echo "<tr>";
             echo "<td class='manage-column'><h4>{$formName}</h4></td>";
-            echo "<td class='manage-column'>{$emailTo}</td>";
-            echo "<td class='manage-column'>{$emailTo2}</td>";
+            echo "<td class='manage-column' style='white-space: nowrap;'>{$emailTo}</td>";
+            echo "<td class='manage-column' style='white-space: nowrap;'>{$emailTo2}</td>";
             echo "<td class='manage-column'>{$recaptcha}</td>";
             echo "<td class='manage-column'>{$recaptcha3}</td>";
             echo "<td class='manage-column'>{$honeypot}</td>";
+            echo "<td class='manage-column'>{$collectSubmissions}</td>";
             echo "<td class='manage-column'>{$formActions}</td>";
             echo "</tr>";
           }
@@ -195,7 +212,8 @@ function euf_admin_notice_missing_main_plugin() {
 
   $message = sprintf(
   /* translators: 1: Plugin name 2: Elementor */
-    esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-test-extension' ),
+    esc_html__( '"%1$s" requires "%2$s" to be installed and activated.',
+      'elementor-test-extension' ),
     '<strong>' . esc_html__( 'Elementor used forms', 'elementor-test-extension' ) . '</strong>',
     '<strong>' . esc_html__( 'Elementor', euf_title() ) . '</strong>'
   );
